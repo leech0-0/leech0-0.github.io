a62fc9afa6a14022020cab42b0c4c8b7c920f5ef
leech0-0 <130814436+leech0-0@users.noreply.github.com>
2024-10-26 11:36:33 +1100

    dropdown menu, looks better but same issue

--- assignment3/graveyardcode.js
+++ assignment3/graveyardcode.js
@@ -1,17 +1,53 @@
-document.getElementById("button1").addEventListener("click", function () {
-  loadImage(
-    "https://i.pinimg.com/474x/a5/ee/0e/a5ee0e6568a56d871750041e73a96584.jpg"
-  );
-});
+//eyes declaring
+const eyeSelectionButton = document.getElementById("button-eyes");
+const eyeColour1 = document.getElementById("eye-colour-1");
+const eyeColour2 = document.getElementById("eye-colour-2");
+const eyeColour3 = document.getElementById("eye-colour-3");
+const eyeColour4 = document.getElementById("eye-colour-4");
+const eyeColour5 = document.getElementById("eye-colour-5");
 
-document.getElementById("button2").addEventListener("click", function () {
-  loadImage(
-    "https://i.pinimg.com/474x/fb/a6/db/fba6db3afbdf5e21e3812df539afe19a.jpg"
-  );
-});
+//EYES
+//blue eyes selected
+const blueEyeSelected = document.getElementById("eyes_blue");
 
-function loadImage(imagePath) {
-  const imgElement = document.getElementById("image");
-  imgElement.src = imagePath;
-  imgElement.style.display = "block"; // Show the image
+eyeColour1.addEventListener("click", chooseEyeColour1);
+
+function chooseEyeColour1() {
+  eyes_blue.style.display = "block";
+}
+
+//brown eyes selected
+const brownEyeSelected = document.getElementById("eyes_brown");
+
+eyeColour2.addEventListener("click", chooseEyeColour2);
+
+function chooseEyeColour2() {
+  eyes_brown.style.display = "block";
+}
+
+//green eyes selected
+const greenEyeSelected = document.getElementById("eyes_green");
+
+eyeColour3.addEventListener("click", chooseEyeColour3);
+
+function chooseEyeColour3() {
+  eyes_green.style.display = "block";
+}
+
+//grey eyes selected
+const greyEyeSelected = document.getElementById("eyes_grey");
+
+eyeColour4.addEventListener("click", chooseEyeColour4);
+
+function chooseEyeColour4() {
+  eyes_grey.style.display = "block";
+}
+
+//pastel blue eyes selected
+const pastelblueEyeSelected = document.getElementById("eyes_pastelblue");
+
+eyeColour5.addEventListener("click", chooseEyeColour5);
+
+function chooseEyeColour5() {
+  eyes_pastelblue.style.display = "block";
 }
